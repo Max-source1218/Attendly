2517a08f959dfd14e0a45a3a238e8a360f162d5d
Max <maxrethcruz@gmail.com>
2026-04-11 14:28:03 +0800

    M

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
@@ -28,6 +28,10 @@ const authenticateToken = (req, res, next) => {
     next();
   });
 };
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "sign-in.html"));
+});
 
 app.use("/api/auth", require('./routes/authRoutes'));
 app.use("/api/classes", authenticateToken, require("./routes/classesRoutes"));
